fa94a2a77a748c2c17cfc43b0e4e466a8cc84d42
bulletcross <bulletcross@gmail.com>
2018-05-28 23:18:06 +0530

    added single and batch generators

--- word2vec/main.py
+++ word2vec/main.py
@@ -4,10 +4,12 @@ import numpy as np
 import tensorflow as tf
 
 from collections import Counter
+import random
 
 DATA = 'Dataset/text8'
 VOCAB = 'Dataset/vocabulary.tsv'
 VOCAB_SIZE = 50
+BATCH_SIZE = 128
 
 def data_generator_initialization():
     # compat = compatibility with python bytes and unicode
@@ -20,6 +22,7 @@ def data_generator_initialization():
         vocab_index = 0
         # Include the most recurring words in list
         vocab_word_list.extend(Counter(all_words).most_common(VOCAB_SIZE-1))
+        index = 0
         for word, _ in vocab_word_list:
             dictionary[word] = index
             index = index + 1
@@ -27,3 +30,32 @@ def data_generator_initialization():
         key_value_dict = dict(zip(dictionary.values(), dictionary.keys()))
     word_index = [dictionary[word] if word in dictionary else 0 for word in all_words]
     return word_index
+
+def single_data_generator(word_index, ctx_window):
+    for index, word in enumerate(word_index):
+        ctx = random.randint(1, ctx_window)
+        for ctx_word in word_index[max(0, index - ctx): index]:
+            yield word, ctx_word
+        for ctx_word in word_index[index + 1: index + ctx + 1]:
+            yield word, ctx_word
+
+
+def batch_data_generator(batch_size, word_index, ctx_window):
+    generator_fn = single_data_generator(word_index, ctx_window)
+    while True:
+        word_batch = np.zeros(BATCH_SIZE, dtype = np.int32)
+        ctx_batch = np.zeros([BATCH_SIZE, 1])
+        # Fill in the batch from single data generator
+        for index in range(batch_size):
+            word_batch[index], ctx_batch[index] = next(generator_fn)
+        yield word_batch, ctx_batch
+
+def main():
+    word_index = data_generator_initialization()
+    gen = single_data_generator(word_index, 2)
+    for i in range(5):
+        w,c = next(gen)
+        print(w, c)
+
+if __name__ == "__main__":
+    main()
